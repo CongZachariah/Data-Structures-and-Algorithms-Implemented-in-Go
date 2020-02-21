22175f8d193f8744ad45f6eceb0edc871da146f2
Zheng Cong <55648528+CongZachariah@users.noreply.github.com>
2020-02-21 08:57:38 +0800

    Update leetcode53.go

--- leetcode53.go
+++ leetcode53.go
@@ -10,18 +10,20 @@ func main() {
 }
 
 func maxSubArray(nums []int) int {
-	max := -2147483647
-	var sum int
-	for l := 0 ; l<len(nums) ; l ++ {
-		for r := l ; r<len(nums) ; r ++ {
-			for t := l ; t<=r ; t ++{
-				sum+=nums[t]
-			}
-			if sum > max {
-				max = sum
-			}
-			sum = 0
+	if len(nums)<2{
+		return nums[0]
+	}
+	var temp int
+	res := -2147483647
+	for i:= 0 ;i<len(nums);i++{
+		if nums[i]>temp+nums[i]{
+			temp = nums[i]
+		}else {
+			temp = temp+nums[i]
+		}
+		if temp>res{
+			res = temp
 		}
 	}
-	return max
+	return res
 }
