540512a9705886d144cf57c991daf21fa4b20bbd
Zheng Cong <55648528+CongZachariah@users.noreply.github.com>
2020-03-07 22:34:07 +0800

    Add files via upload

---
+++ leetcode interview10-2.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Print(numWays(11))
+}
+
+func numWays(n int) int {
+	if n==0{
+		return 1
+	}
+	if n==2||n==1{
+		return n
+	}
+	dp:=make([]int,n+1)
+	dp[1]=1
+	dp[2]=2
+	for i:=3;i<=n;i++{
+		dp[i]=(dp[i-1]+dp[i-2])%1000000007
+	}
+	return dp[n]
+}
