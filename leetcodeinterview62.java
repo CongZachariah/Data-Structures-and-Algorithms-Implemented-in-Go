1e7aeb8ea218b204e431f7b2f18bc56af9152957
Zheng Cong <55648528+CongZachariah@users.noreply.github.com>
2020-03-30 17:05:07 +0800

    Add files via upload

---
+++ leetcodeinterview62.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class leetcodeinterview62 {
+    public static int lastRemaining(int n, int m) {
+        ArrayList<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int idx = 0;
+        while (n > 1) {
+            idx = (idx + m - 1) % n;
+            list.remove(idx);
+            n--;
+        }
+        return list.get(0);
+    }
+
+    public static void main(String[] args){
+        int res = lastRemaining(15,3);
+        System.out.println(res);
+    }
+}
